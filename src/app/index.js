27fe9b4699447a61752f0c66f6e67fcc88798d7e
braedan <braedantye@msn.com>
2018-06-28 14:34:36 -0400

    '00'

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,65 @@
-import React from "react";
-import {render} from "react-dom";
+// import React from "react";
+// import {render} from "react-dom";
+//
+// import { User } from './components/User';
+// import { Main } from './components/Main';
+//
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: "Max"
+//         };
+//     }
+//
+//     changeUsername(newName) {
+//         this.setState({
+//             username: newName
+//         });
+//     }
+//
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this.changeUsername.bind(this)}/>
+//                 <User username={this.state.username}/>
+//             </div>
+//         );
+//     }
+// }
+//
+// render(<App />, window.document.getElementById('app'));
+import { createStore } from 'redux';
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBTRACT":
+      state = state - action.payload;
+      break;
+  }
+  return state;
+};
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
-        };
-    }
+const store = createStore(reducer, 1);
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
+store.subscribe(() => {
+  console.log("Store updated!", store.getState());
+});
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
-}
+store.dispatch({
+  type: "ADD",
+  payload: 110
+});
 
-render(<App />, window.document.getElementById('app'));
+store.dispatch({
+  type: "ADD",
+  payload: 22
+});
+
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 80
+});
